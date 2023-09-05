c22734eef38a83365c62f7e6fac24e8ef1f12197
emirbeknurbekov <emirbek.nurbekov@alatoo.edu.kg>
2023-09-06 01:32:14 +0600

    carousel added

--- src/components/home/Carousel.jsx/CarouselHomePage.jsx
+++ src/components/home/Carousel.jsx/CarouselHomePage.jsx
@@ -3,25 +3,32 @@ import Carousel from "react-bootstrap/Carousel";
 import carouselImageFirst from "../../../images/carousel-image1.svg";
 import carouselImageSecond from "../../../images/carousel-image-second.jpg";
 
+import "./CarouselHomePage.css";
+
 const CarouselHomePage = () => {
   return (
     <Carousel>
       <Carousel.Item>
-        <img className="d-block w-100" src={carouselImageFirst} alt="error" />
+        <img className="carousel-image" src={carouselImageFirst} alt="error" />
         <Carousel.Caption>
-          <h4>T-shirt / Tops</h4>
-          <h2>Summer Value Pack</h2>
-          <h4>cool / colorful / comfy</h4>
-          <button>Shop Now</button>
+          <div className="carousel__descr">
+            <h4 className="carousel-header">T-shirt / Tops</h4>
+            <h2 className="carousel-subheader">Summer Value Pack</h2>
+            <h4 className="carousel-title">cool / colorful / comfy</h4>
+            <button className="carousel__btn">Shop Now</button>
+          </div>
         </Carousel.Caption>
       </Carousel.Item>
+
       <Carousel.Item>
-        <img className="d-block w-100 " src={carouselImageSecond} alt="error" />
+        <img className="carousel-image" src={carouselImageSecond} alt="error" />
         <Carousel.Caption>
-          <h4>T-shirt / Tops</h4>
-          <h2>Summer Value Pack</h2>
-          <h4>cool / colorful / comfy</h4>
-          <button>Shop Now</button>
+          <div className="carousel__descr">
+            <h4 className="carousel-header">T-shirt / Tops</h4>
+            <h2 className="carousel-subheader">Summer Value Pack</h2>
+            <h4 className="carousel-title">cool / colorful / comfy</h4>
+            <button className="carousel__btn">Shop Now</button>
+          </div>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
