da57cb87efcd58eacbcd9b57df6bbab59825459e
emirbeknurbekov <emirbek.nurbekov@alatoo.edu.kg>
2023-09-06 02:28:41 +0600

    second block home-page added

--- src/components/home/Carousel/Carousel.jsx
+++ src/components/home/Carousel/Carousel.jsx
@@ -3,9 +3,12 @@ import Carousel from "react-bootstrap/Carousel";
 import carouselImageFirst from "../../../images/carousel-image1.svg";
 import carouselImageSecond from "../../../images/carousel-image-second.jpg";
 
-import "./CarouselHomePage.css";
+import { useNavigate } from "react-router-dom";
+
+import "./Carousel.css";
 
 const CarouselHomePage = () => {
+  const navigate = useNavigate();
   return (
     <Carousel>
       <Carousel.Item>
@@ -27,7 +30,12 @@ const CarouselHomePage = () => {
             <h4 className="carousel-header">T-shirt / Tops</h4>
             <h2 className="carousel-subheader">Summer Value Pack</h2>
             <h4 className="carousel-title">cool / colorful / comfy</h4>
-            <button className="carousel__btn">Shop Now</button>
+            <button
+              onClick={() => navigate("/products")}
+              className="carousel__btn"
+            >
+              Shop Now
+            </button>
           </div>
         </Carousel.Caption>
       </Carousel.Item>
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./HomePage.css";
-import CarouselHomePage from "../../components/home/Carousel.jsx/CarouselHomePage";
+import CarouselHomePage from "../../components/home/Carousel/Carousel";
 
 const HomePage = () => {
   return (
     <>
       <CarouselHomePage />
+      <div className="home-pages__banners__block">
+        <div className="home-pages__banners__block_first"></div>
+        <div className="home-pages__banners__block_second"></div>
+      </div>
     </>
   );
 };
